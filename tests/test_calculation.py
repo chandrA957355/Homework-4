e254ebe5a84252a17945ddb9797c9609c23489ea
chandrA957355 <yk339@njit.edu>
2024-09-28 16:08:18 -0400

    Pylint and coverage changes

--- tests/test_calculation.py
+++ tests/test_calculation.py
@@ -1,7 +1,8 @@
-from calculator.calculation import Calculation
-from calculator.operations import add,subtract,multiply,divide
+""" Calculation Test Module """
 from decimal import Decimal
 import pytest
+from calculator.calculation import Calculation
+from calculator.operations import add,subtract,multiply,divide
 
 
 @pytest.mark.parametrize("n1, n2, operation, expected",
@@ -13,18 +14,26 @@ import pytest
     (Decimal('3.0'), Decimal('2.0'), add, Decimal('5.0'))
 ])
 
+
 def test_operate(n1, n2, operation, expected):
+    """
+    test for operate method to encapsulate the decimals and operation to call the operate method to perform arithmetic operation and compare the expected value.
+    """
     calc = Calculation(n1, n2, operation)
     assert calc.operate() == expected, "operation failed!"
 
 def test_strrepr():
+    """
+    Test for string representation
+    """
     calc = Calculation(Decimal('2'), Decimal('2'), add)
     str_match = "Calculation(2, 2, add)"
     assert calc.__strrepr__() == str_match, f"Expected {str_match} not equal to {calc.__strrepr__()}."
 
-
 def test_dividebyzero():
-    
+    """
+    Test for condition of division by zero
+    """
     calc = Calculation(Decimal('5'), Decimal('0'), divide)
     with pytest.raises(ValueError, match="Error : Divide By Zero occured!"):
         calc.operate()
--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -8,7 +8,9 @@ from calculator.operations import add, subtract, multiply, divide
 
 @pytest.fixture
 def sample_operations():
-
+    '''
+    Adding the sample operations into the history for test cases
+    '''
     calculations.delete_calculation()
 
     calculations.add_calculation(Calculation(Decimal('2'), Decimal('3'), add))
@@ -17,28 +19,46 @@ def sample_operations():
     calculations.add_calculation(Calculation(Decimal('9'), Decimal('3'), divide))
 
 def test_addcalculation():
+    '''
+    testing the history for the calculation addition
+    '''
     add_obj = Calculation(Decimal('5'),Decimal('3'), add)
     calculations.add_calculation(add_obj)
     assert calculations.get_latest() == add_obj, "History addition failed."
 
 def test_getallhistory(sample_operations):
+    '''
+    Checking the count of history entries to verify the get history 
+    '''
     history = calculations.print_all_calculation()
     assert len(history) == 4, "All history count is not correct."
 
 def test_deletehistory():
+    '''
+    checking the clear function on the history
+    '''
     calculations.delete_calculation()
     assert len(calculations.print_all_calculation()) == 0, "Delete history failed."
 
 def test_getlatestcalculation(sample_operations):
+    '''
+    Testing if the last calculation is the latest in the history list
+    '''
     latest = calculations.get_latest()
     assert latest.a == Decimal('9') and latest.b == Decimal('3'), "Get latest failed."
 
 def test_getlatestafterclear():
+    '''
+    testing if the history is empty after clear
+    '''
     calculations.delete_calculation()
-    assert calculations.get_latest() is None, "Get Latest after clear is not working"
+    assert calculations.get_latest() is None, "Get history should be empty"
 
 def test_findbyoperation(sample_operations):
+    '''
+    Tests the history to get the operations list with given operation
+    '''
     add_find = calculations.filter_with_operation("add")
     assert len(add_find) == 1, "count of add operations doesnt match"
-    getbymultiply_operation = calculations.filter_with_operation("multiply")
-    assert len(add_find) == 1, "count of multiply operations doesnt match"
+    multiply_filter = calculations.filter_with_operation("multiply")
+    assert len(multiply_filter) == 1, "count of multiply operations doesnt match"
